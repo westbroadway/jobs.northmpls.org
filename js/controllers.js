04aec654d169de8c84634e13f96b409d3173eac3
Kai Curry <kai@sundaysenergy.com>
2013-04-19 00:45:26 -0500

    adding notes

--- js/controllers.js
+++ js/controllers.js
@@ -5,14 +5,21 @@
 angular.module('myApp.controllers', [])
 
   .controller('MyCtrl1', function($scope, $http) {
+    // Each controller should share a single object that is only requested once.
     $http.get('http://northmpls.webscript.io/jobs').success(function (response) {
+      // The entry should be the based on the argument that is passed.
+      // The argument is entry.jobkey.
+      // Search array of objects of jobkey thath matches.
       $scope.entry = response.entries[0];
+
+      // If the object has a fulltext field it should be sent through markdown converter.
       var mdConverter = new Showdown.converter();
-      $scope.entry.Field7 = mdConverter.makeHtml($scope.entry.Field7);
+      $scope.entry.fulltext = mdConverter.makeHtml($scope.entry.fulltext);
     })
   })
 
   .controller('JobList', function($scope, $http) {
+    // Each controller should share a single object that is only requested once.
     $http.get('http://northmpls.webscript.io/jobs').success(function (response) {
       $scope.entries = response;
     })
