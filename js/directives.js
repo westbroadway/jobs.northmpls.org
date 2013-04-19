8c60024c377b786c5be2c5fda5caafef1c5c2040
Dmitry Evseev <kradmiy@gmail.com>
2013-04-19 14:46:01 +0300

    Introduces loading progress notification directive

--- js/directives.js
+++ js/directives.js
@@ -3,9 +3,18 @@
 /* Directives */
 
 
-angular.module('jobsNorthmpls.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+angular.module('jobsNorthmpls.directives', [])
+  .directive('resourceLoadingNotification', function factory ($http) {
+    'use strict';
+
+    return {
+      template: '<div class="loading label" ng-hide="isLoadingAlertHidden()">Loading...</div>',
+      replace: true,
+      restrict: 'A',
+      link: function postLink (scope, iElement, iAttrs) {
+        scope.isLoadingAlertHidden = function () {
+          return $http.pendingRequests.length === 0;
+        };
+      }
     };
-  }]);
+  });
