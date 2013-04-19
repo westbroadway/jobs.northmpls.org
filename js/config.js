677922f002daa87ffcd6dff403bebbabedec57df
Dmitry Evseev <kradmiy@gmail.com>
2013-04-19 12:55:45 +0300

    Introduces config into angular project

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('jobsNorthmpls.constants')
+angular.module('jobsNorthmpls.constants', [])
   .constant('config', {
     JOB_ENTRIES_SOURCE: 'http://northmpls.webscript.io/jobs'
   });
