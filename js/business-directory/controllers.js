d4923d82aada3b2b0b3f0a51754d9c39d11dd01a
Dmitry Evseev <kradmiy@gmail.com>
2013-04-26 23:27:33 +0300

    Show link for item only if it's an actual link

--- js/business-directory/controllers.js
+++ js/business-directory/controllers.js
@@ -6,6 +6,11 @@ angular.module('businessDirectory.controllers', [])
 
   .controller('MainCtrl', function ($scope, config, Businesses) {
     Businesses.getAll();
+
+    var regexpUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+    $scope.isLink = function (string) {
+      return regexpUrl.test(string);
+    };
   })
 
   .controller('BusinessListCtrl', function ($scope) {
