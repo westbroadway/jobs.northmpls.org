9e24f19f8d0883699728cd815f505894805a1d23
Dmitry Evseev <kradmiy@gmail.com>
2013-04-29 21:47:20 +0300

    WIP on enabling info popup on map markers

--- js/business-directory/controllers.js
+++ js/business-directory/controllers.js
@@ -69,12 +69,6 @@ angular.module('businessDirectory.controllers', [])
 
     $scope.mapLoaded = function () {
       // adds new marker to map
-      /*
-       $scope.markers.push(new google.maps.Marker({
-       map: $scope.map,
-       position: new google.maps.LatLng(35.784, -78.670)
-       }));
-       */
     };
 
     $scope.getMapOptions = function () {
@@ -132,6 +126,7 @@ angular.module('businessDirectory.controllers', [])
             map: $scope.map,
             position: new google.maps.LatLng(lat, lon)
           });
+          $scope.markers.push(business.marker);
         }
       });
 
@@ -140,4 +135,36 @@ angular.module('businessDirectory.controllers', [])
         $scope.activeCategories[index] = true;
       });
     });
+  })
+  .controller('TestCalendar', function ($scope) {
+    $scope.myMarkers = [];
+
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(35.784, -78.670),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.addMarker = function ($event) {
+      $scope.myMarkers.push(new google.maps.Marker({
+        map: $scope.myMap,
+        position: $event.latLng
+      }));
+    };
+
+    $scope.setZoomMessage = function (zoom) {
+      $scope.zoomMessage = 'You just zoomed to ' + zoom + '!';
+      console.log(zoom, 'zoomed');
+    };
+
+    $scope.openMarkerInfo = function (marker) {
+      $scope.currentMarker = marker;
+      $scope.currentMarkerLat = marker.getPosition().lat();
+      $scope.currentMarkerLng = marker.getPosition().lng();
+      $scope.myInfoWindow.open($scope.myMap, marker);
+    };
+
+    $scope.setMarkerPosition = function (marker, lat, lng) {
+      marker.setPosition(new google.maps.LatLng(lat, lng));
+    };
   });
